226d58194637a57124dcc6b66456f8a5f6aeba79
nciampelletti <natashat@shaw.ca>
2022-07-14 13:40:59 -0700

    setup redux toolkit slice and store

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import "normalize.css"
 import "./index.css"
 import App from "./App"
 
+import { Provider } from "react-redux"
+import { store } from "./store"
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <App tab='home' />
+  </Provider>
 )
